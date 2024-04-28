11507bec84bdf38d0878bf019e4e97916f6892ae
roycesun7 <roycesun@sas.upenn.edu>
2024-04-28 15:15:06 -0400

    production fixes

--- pet-gallery/src/App.tsx
+++ pet-gallery/src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
--- pet-gallery/src/components/pages/About.tsx
+++ pet-gallery/src/components/pages/About.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Navbar from '../common/Navbar';
 
 const AboutPage = () => {
